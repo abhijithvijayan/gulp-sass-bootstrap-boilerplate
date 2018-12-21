5155b8f4666ee8e4101dcd84b8a269e1df6c3707
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2018-12-21 22:19:06 +0530

    major changes

--- gulpfile.js
+++ gulpfile.js
@@ -5,41 +5,59 @@ const autoprefixer = require("gulp-autoprefixer");
 const sourcemaps = require("gulp-sourcemaps");
 const concat = require("gulp-concat");
 const cleanCSS = require("gulp-clean-css");
-const hash = require("gulp-hash");
 const clean = require("gulp-clean");
+const filter = require("gulp-filter");
+const rev = require("gulp-rev");
+const revRewrite = require("gulp-rev-rewrite");
+
+function hash_reference() {
+
+  gulp
+    .src([cssDest + "/*.css", jsDest + "/*.js"], { base: "assets" })
+    .pipe(rev())
+    .pipe(gulp.dest('assets'))  // write rev'd assets to build dir
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('./dist'))  // write manifest to build dir
+
+    
+    var manifester = () => {
+    const manifest = gulp.src('./dist/rev-manifest.json');
+    gulp
+        .src('./dist/*.html')
+        // .src(['./dist/*.html'], { base: "assets" })
+        .pipe(revRewrite({ manifest }))
+        .pipe(gulp.dest('./dist'));
+    }
+    manifester();
+//   // cleanFiles('./dist/assets')
+}
+
 
 //style paths
 var sassFiles = "./app/sass/*.scss",
   cssDest = "./dist/assets/css/",
-  htmlFiles = "./dist/*.html",
-  jsFiles = "./dist/assets/js";
+  htmlFiles = "./app/*.html",
+  jsDest = "./dist/assets/js";
 
+// deleting compiled files
 function cleanFiles(loc) {
-    return gulp.src(loc, { read: false })
-        .pipe(clean());
+  return gulp.src(loc, { read: false }).pipe(clean());
 }
 
 gulp.task("js", () => {
-  cleanFiles(jsFiles + '/*');
-  return ( 
+  cleanFiles(jsDest + "/*");
+  return (
     gulp
-    .src("./app/main.js")
-    .pipe(hash()) // Add hashes to the files' names
-    .pipe(gulp.dest(jsFiles)) // Write the renamed files
-    .pipe(
-      hash.manifest("./dist/assets/js-manifest.json", {
-        deleteOld: true,
-        sourceDir: __dirname + jsFiles
-      })
-    )
-    // writes the manifest file
-    .pipe(gulp.dest("."))
+      .src("./app/main.js")
+      .pipe(concat("bundle.js"))
+      // .pipe(rev())  // Add hashes to the files' names
+      .pipe(gulp.dest(jsDest)) // Write the renamed files
   );
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task("sass", () => {
-  cleanFiles(cssDest + '/*');
+  cleanFiles(cssDest + "/*");
   return (
     gulp
       .src(sassFiles)
@@ -50,17 +68,8 @@ gulp.task("sass", () => {
       .pipe(cleanCSS())
       .pipe(sourcemaps.write())
 
-      .pipe(hash())
+      //   .pipe(rev())
       .pipe(gulp.dest(cssDest))
-      .pipe(
-        hash.manifest("./dist/assets/css-manifest.json", {
-          // Generate the manifest file
-          deleteOld: true,
-          sourceDir: __dirname + cssDest
-        })
-      )
-      // writes the manifest file
-      .pipe(gulp.dest("."))
 
       .pipe(browserSync.stream())
   );
@@ -68,17 +77,26 @@ gulp.task("sass", () => {
 
 // Static Server + watching scss/html files
 gulp.task("serve", () => {
+  // copy html files
+  gulp
+    .src(htmlFiles)
+    .pipe(gulp.dest("./dist"));
+  // hashing
+  hash_reference();
   browserSync.init({
     server: {
-      baseDir: "./app/"
+      baseDir: "./dist/"
     }
   });
   // watch functions
   gulp.watch(sassFiles, gulp.series("sass"));
-  gulp.watch('./app/main.js', gulp.series("js"));
+  gulp.watch("./app/main.js", gulp.series("js"));
   gulp.watch(htmlFiles).on("change", browserSync.reload);
 });
 
+// default task
 gulp.task(
   "default",
-  gulp.series(gulp.parallel("sass", "js"), "serve"), () => {});
+  gulp.series(gulp.parallel("sass", "js"), "serve"),
+  () => {}
+);
