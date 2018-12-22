5e9dfa68a0f03309b331b77f1be238adf112694d
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2018-12-22 08:56:23 +0530

    hashing fixes

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const concat = require("gulp-concat");
 const clean = require("gulp-clean");
 const rev = require("gulp-rev");
 const revRewrite = require("gulp-rev-rewrite");
-const babel = require('gulp-babel');
+const babel = require("gulp-babel");
 
 //style paths
 var sassDir = "./app/sass/*.scss",
@@ -15,26 +15,22 @@ var sassDir = "./app/sass/*.scss",
   jsDest = "./build/assets/js",
   assets = "./build/assets";
 
-// function hash_reference() {
-gulp.task(
-  "rev",
-  gulp.series(function(done) {
+// hashing task
+gulp.task("rev", function() {
+  return (
     gulp
       .src([cssDest + "/*.css", jsDest + "/*.js"], { base: "./build/assets" })
-      // hashing
       .pipe(rev())
       .pipe(gulp.dest(assets)) // write rev'd assets to build dir
       .pipe(rev.manifest())
-      .pipe(gulp.dest("./build")); // write manifest to build dir
-
-    //   manifester();
-    // cleanFiles('./build/assets')
-    done();
-  })
-);
+      .pipe(gulp.dest("./build")) // write manifest to build dir
+  );
+  // cleanFiles('./build/assets')
+});
 
 gulp.task(
-  "rev-rewrite", gulp.series(function(done) {
+  "rev-rewrite",
+  gulp.series("rev", function(done) {
     const manifest = gulp.src("./build/rev-manifest.json");
     gulp
       .src("./build/*.html")
@@ -61,55 +57,59 @@ gulp.task("sass", () => {
       .src(sassDir)
       .pipe(sass())
       .pipe(concat("style.min.css"))
-
-      //   .pipe(rev())
       .pipe(gulp.dest(cssDest))
-
       .pipe(browserSync.stream())
   );
 });
 
+// babel build task
 gulp.task("js", () => {
   // clean js build files
   cleanFiles(jsDest + "/*");
 
-  return (
-    gulp
-      .src(jsDir)
-      .pipe(babel({
-        presets: ['@babel/env']
-       }))
-      .pipe(concat("bundle.js"))
-      // .pipe(rev())  // Add hashes to the files' names
-      .pipe(gulp.dest(jsDest)) // Write the renamed files
-  );
+  return gulp
+    .src(jsDir)
+    .pipe(
+      babel({
+        presets: ["@babel/env"]
+      })
+    )
+    .pipe(concat("bundle.js"))
+    .pipe(gulp.dest(jsDest)); // Write the renamed files
 });
 
-
-gulp.task("html", gulp.series(function(done) {
-      // copy html files to build dir
-    gulp.src(htmlDir)
-        .pipe(gulp.dest("./build"));
+gulp.task(
+  "html",
+  gulp.series(function(done) {
+    // copy html files to build dir
+    gulp.src(htmlDir).pipe(gulp.dest("./build"));
     done();
-}));
+  })
+);
 
-// Static Server + watching scss/html files
+// Static Server
 gulp.task("serve", () => {
-
-  // hashing
-  //   hash_reference();
-
   browserSync.init({
     server: {
       baseDir: "./build/"
     }
   });
+});
 
-  // watch functions
+// watching js/scss/html files
+gulp.task("watch", function() {
+  // watch functions (to be corrected)
   gulp.watch(sassDir, gulp.series("sass"));
   gulp.watch("./app/main.js", gulp.series("js"));
   gulp.watch(htmlDir).on("change", browserSync.reload);
 });
 
 // default task
-gulp.task("default", gulp.series(gulp.parallel("sass", "js"), "serve"));
+gulp.task(
+  "default",
+  gulp.series(
+    gulp.parallel("html", "sass", "js"),
+    gulp.series("rev-rewrite"),
+    "serve"
+  )
+);
